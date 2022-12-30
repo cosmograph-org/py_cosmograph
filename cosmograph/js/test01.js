06dfb7c1d719c509876774997b8a8eb4addccc67
Thor Whalen <thorwhalen1@gmail.com>
2022-12-30 13:27:23 +0000

    feat: js_bridge handles more function definition types and is now covered with some tests

--- cosmograph/js/test01.js
+++ cosmograph/js/test01.js
@@ -11,7 +11,20 @@ function bar(green, eggs = 'food', and= true, ham= 4) {
     if (and) return eggs.length * ham
 }
 
-// function assigned to a variable of window
+add_one = function (x) {
+    return x + 1
+}
+
+let with_let = function (x) {
+    return x + 2
+}
+
+const with_const = function (x) {
+    return x + 2
+}
+
+
+// function assigned to a property of window
 window.SetData = function (id, nodes, links) {
     const graph = Graphs.get(id)
     if (graph) graph.setData(nodes, links)
--- cosmograph/js_bridge.py
+++ cosmograph/js_bridge.py
@@ -1,25 +1,28 @@
 """Utils to bridge with JS"""
 
 import os
+from typing import Any
 from pathlib import Path
 from functools import partial
-from dol.signatures import Sig
+from itertools import chain
 
+from dol.signatures import Sig
 import esprima
 
-AstTree = esprima.nodes.Script
+AstScript = esprima.nodes.Script
+AstNode = esprima.nodes.Node
 
 
 # TODO: Parse out js docs and add them to py mirror function
 
 
-def parse_js_code(js_code: str, encoding: str | None = None) -> AstTree:
+def parse_js_code(js_code: str, encoding: str | None = None) -> AstScript:
     if os.path.isfile(js_code):
         js_code = Path(js_code).read_text(encoding=encoding)
     return esprima.parse(js_code)
 
 
-def _extract_params(x):
+def _extract_params(x: AstNode):
     if x.type == 'Identifier':
         return dict(name=x.name)
     elif x.type == 'AssignmentPattern':
@@ -28,14 +31,48 @@ def _extract_params(x):
         raise ValueError(f'Unknown param type: {x.type=}')
 
 
+def _extract_name_from_assignment_expression(x: AstNode):
+    if x.left.type == 'Identifier':
+        return x.left.name
+    elif x.left.type == 'MemberExpression':
+        return f'{x.left.object.name}.{x.left.property.name}'
+    else:
+        raise ValueError(f'Unknown AssignmentExpression type: {x.type=}')
+
+
 def extract_js_func_params(params_list):
     return map(_extract_params, params_list)
 
 
-def function_signatures(t: AstTree):
-    for x in t.body:
-        if x.type == 'FunctionDeclaration':
-            yield x.id.name, list(extract_js_func_params(x.params))
+def _extract_params_from_function_expression(x):
+    return extract_js_func_params(x.params)
+
+
+def extract_func_name_and_params(ast_node: AstNode):
+    """Extract one or several function ``(name, params)`` pair(s) from an AST node"""
+    x = ast_node
+    if x.type == 'FunctionDeclaration':
+        yield x.id.name, list(extract_js_func_params(x.params))
+    elif x.type == 'AssignmentExpression':
+        yield (
+            _extract_name_from_assignment_expression(x),
+            list(extract_js_func_params(x.right.params)),
+        )
+    elif x.type == 'VariableDeclarator':
+        yield x.id.name, list(extract_js_func_params(x.init.params))
+    elif x.type == 'VariableDeclaration':
+        # Here we may have several declarations, so we use yield from
+        yield from chain.from_iterable(
+            map(extract_func_name_and_params, x.declarations)
+        )
+    elif x.type == 'ExpressionStatement':
+        yield from extract_func_name_and_params(x.expression)
+
+
+def func_name_and_params_pairs(js_code: str, *, encoding=None):
+    ast_script = parse_js_code(js_code, encoding=encoding)
+    for ast_node in ast_script.body:
+        yield from extract_func_name_and_params(ast_node)
 
 
 def dflt_py_to_js_value_trans(x):
@@ -48,12 +85,12 @@ def dflt_py_to_js_value_trans(x):
 
 # TODO: Add value transformer (routing). For example, booleans, True->true
 def _js_func_call(
-        *args,
-        __sig,
-        __func_call_template,
-        __value_trans=dflt_py_to_js_value_trans,
-        __apply_defaults=True,
-        **kwargs
+    *args,
+    __sig,
+    __func_call_template,
+    __value_trans=dflt_py_to_js_value_trans,
+    __apply_defaults=True,
+    **kwargs,
 ):
     _kwargs = __sig.kwargs_from_args_and_kwargs(
         args, kwargs, apply_defaults=__apply_defaults
@@ -64,13 +101,19 @@ def _js_func_call(
 
 
 def mk_py_binder_func(
-        name, func_sig, *, prefix='', suffix='', doc='',
-        value_trans=dflt_py_to_js_value_trans,
-        apply_defaults=True,
+    name,
+    params,
+    *,
+    prefix='',
+    suffix='',
+    doc='',
+    value_trans=dflt_py_to_js_value_trans,
+    apply_defaults=True,
 ):
+    *_, func_name = name.split('.')  # e.g. object.containing.func --> func
     func_call_template = prefix + name + '({inputs})' + suffix
 
-    sig = Sig.from_params(func_sig)
+    sig = Sig.from_params(params)
     js_func_call = partial(
         _js_func_call,
         __sig=sig,
@@ -79,27 +122,51 @@ def mk_py_binder_func(
         __apply_defaults=apply_defaults,
     )
     js_func_call = sig(js_func_call)
-    js_func_call.__name__ = name
+    js_func_call.__name__ = func_name
     js_func_call.__doc__ = doc
 
     return js_func_call
 
 
 class JsBridge:
-    """A class that will contain methods that mirror JS function calls"""
+    """
+    The default class to make instances that will contain methods that mirror JS
+    function calls
+    """
 
 
-def js_code_to_py_methods(
-    js_code: str, *, name: str | None = None, encoding: str | None = None
+def add_js_call_attributes_to_obj(
+    js_code: str,
+    *,
+    obj: Any = None,
+    name: str | None = None,
+    encoding: str | None = None,
+    forbidden_method_names=(),
+    apply_defaults=True
 ):
-    t = parse_js_code(js_code, encoding=encoding)
+    """Add js call functions as attributes to an object (by default a new JsBridge
+    instance)."""
+    if obj is None:
+        obj = JsBridge()
 
-    js = JsBridge()
+    forbidden_method_names = set(forbidden_method_names)
 
-    for name, func_sig in function_signatures(t):
-        setattr(js, name, mk_py_binder_func(name, func_sig))
+    for full_func_ref, params in func_name_and_params_pairs(js_code, encoding=encoding):
+        *_, func_name = full_func_ref.split('.')  # e.g. object.containing.func --> func
+        # TODO: Could specify a recovery function that finds another name instead of
+        #  raising an error
+        if func_name in forbidden_method_names:
+            raise ValueError(
+                f"This func name was already used, or mentioned in "
+                f"the forbidden_method_names argument: {func_name}"
+            )
+        setattr(
+            obj,
+            func_name,
+            mk_py_binder_func(full_func_ref, params, apply_defaults=apply_defaults)
+        )
 
     if name:
-        js.__name__ = name
+        obj.__name__ = name
 
-    return js
+    return obj
