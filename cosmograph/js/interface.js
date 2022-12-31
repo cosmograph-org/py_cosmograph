91dbb73a5ff522002acca3e14503a1f72d3500a1
Thor Whalen <thorwhalen1@gmail.com>
2022-12-31 17:16:01 +0000

    feat: removed window from set_data definition

--- cosmograph/js/interface.js
+++ cosmograph/js/interface.js
@@ -1,6 +1,6 @@
 
 
-window.set_data = function(canvas_id, links, nodes) {
+set_data = function(canvas_id, links, nodes) {
     // Note: nodes and links inverted
     if (window.SetData) window.SetData(canvas_id, nodes, links)
 }
