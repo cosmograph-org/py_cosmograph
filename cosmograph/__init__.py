d991a07a28d48e86ddd62b1df9e77087c3fd1dd5
Thor Whalen <thorwhalen1@gmail.com>
2023-02-06 16:00:51 +0100

    fix: correct import

--- cosmograph/__init__.py
+++ cosmograph/__init__.py
@@ -1,5 +1,5 @@
 """Python binders to cosmograph functionality"""
 
 from cosmograph.util import cosmo
-from cosmograph.datasrc.minidot import mini_dot_to_graph_jdict
+from linked.datasrc import mini_dot_to_graph_jdict
 
