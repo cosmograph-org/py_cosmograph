a1585fa987b1df9f03f43fbca75a61f9ef08396b
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-12-09 10:59:11 +0100

    experiment: Showing two versions of CosmoArguments.points_and_links

--- cosmograph/base.py
+++ cosmograph/base.py
@@ -1,5 +1,119 @@
 """Base functionality of cosmograph"""
 
+from i2.doc_mint import inject_docstring_content
+from cosmograph_widget import Cosmograph
+
+from cosmograph._resources import configs_dacc
+
+cosmo_base_sig = configs_dacc.cosmograph_base_signature()
+cosmo_base_params_doc_str = configs_dacc.cosmograph_base_docs()
+
+
+@inject_docstring_content(cosmo_base_params_doc_str, position=-1)
+@cosmo_base_sig
+def base_cosmo(**kwargs):
+    """
+    Thin layer over CosmographWidget to provide a base interface to the widget object
+    """
+    return Cosmograph(**kwargs)
+
+
+@inject_docstring_content(cosmo_base_params_doc_str, position=-1)
+@cosmo_base_sig
+def cosmo(data=None, **kwargs):
+    """
+    Thin layer over CosmographWidget to provide a base interface to the widget object
+    """
+    return Cosmograph(**kwargs)
+
+
+def process_cosmo_input(data, **kwargs):
+    kw = CosmoArguments(data=data, **kwargs)
+    if data is not None and kw.non_nones({'points', 'links'}):
+        raise ValueError(
+            "Cannot specify `points` and `links` if you've specified `data`"
+        )
+    return data
+
+
+class CosmoArguments:
+    """
+    Container for cosmograph arguments, along with diagnosis and processing methods
+    """
+    def __init__(self, data=None, **kwargs):
+        self.data = data
+        self.kwargs = kwargs
+
+    def points_and_links(self):
+        points, links = self.kwargs['points'], self.kwargs['links']
+
+        if data is not None:
+            if self.all_are_none({'points', 'links'}):
+                points = data  # default is to use data as points (and links=None)
+                return points, None 
+            elif self.none_are_none({'points', 'links'}): 
+                raise ValueError(
+                    "Cannot specify `points` and `links` if you've specified `data`"
+                )
+            elif 'links' in self.non_none_names:
+                points = data
+                return points, self.kwargs['links']
+            elif 'points' in self.non_none_names:
+                links = data
+                return self.kwargs['points'], links
+        else:
+            return self.kwargs.get('points'), self.kwargs.get('links')
+    
+    
+    def points_and_links(self):
+        points, links = self.get(['points', 'links'], default=None)
+
+        if data is not None:
+            if points is None and links is None:
+                # If neither points or links are given, use data as points
+                points = data  
+                return points, None 
+            elif points is None and links is not None:
+                # If only links are given, use data as points
+                points = data
+                return points, self.get('links')
+            elif points is not None and links is None:
+                # If only points are given, use data as links
+                links = data
+                return self.get('points'), links
+            else:
+                # If both points and links are given, raise an error
+                raise ValueError(
+                    "Cannot specify `points` and `links` if you've specified `data`"
+                )
+        else:
+            return self.get('points'), self.get('links')
+    
+
+    @cached_property
+    def non_none_names(self):
+        return {k for k, v in self.kwargs.items() if v is not None}
+    
+    def get(self, name, default=None):
+        if isinstance(name, (list, tuple)):
+            return [self.kwargs.get(k, default) for k in name]
+        return self.kwargs.get(name, default)
+
+    def non_nones(self, names):
+        return self.non_nones_names & set(names)
+    
+    def all_are_none(self, names):
+        return self.non_nones(names) == set()
+    
+    def none_are_none(self, names):
+        return len(self.non_nones(names)) == len(names)
+    
+
+
+
+# --------------------------------------------------------------------------------------
+# Some old code that might be useful
+
 from typing import List, Optional, Tuple
 from pydantic import BaseModel
 
@@ -41,6 +155,7 @@ html_code_data_def_template = '''
     graph.fitView();
 </script>
 '''
+
 
 # TODO: Function should be moved to a better place
 def mk_html_code(data):
