a2a446100068e2ea0dbad609a0c3dca5386d0156
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-12-18 18:28:39 +0100

    refactor: remove unused import from __init__.py

--- cosmograph/__init__.py
+++ cosmograph/__init__.py
@@ -1,5 +1,4 @@
 """Python binders to cosmograph functionality"""
 
-from linked.datasrc import mini_dot_to_graph_jdict
 from cosmograph.base import cosmo, base_cosmo
 from cosmograph_widget import Cosmograph
