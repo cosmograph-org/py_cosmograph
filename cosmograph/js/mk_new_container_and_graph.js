2da1e8b831728e3be585f2e54a21fc5c4c855a35
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-01-31 12:40:19 +0100

    refactor: delete js folder

--- cosmograph/js/mk_new_container_and_graph.js
+++
@@ -1,8 +0,0 @@
-globalThis.CreateContainerAndCosmographById = function (id, height, width, config) {
-  const container = document.createElement("div")
-  container.style.height = height;
-  container.style.width = width;
-  GraphContainers.set(id, container)
-  const graph = new cosmograph.Cosmograph(container, config)
-  Graphs.set(id, graph)
-}
--- cosmograph/js/old/mk_new_canvas_and_cosmos_instance.js
+++
@@ -1,8 +0,0 @@
-globalThis.CreateCanvasAndCosmosById = function (id, height, width) {
-    const canvas = document.createElement("canvas")
-    canvas.style.height = height;
-    canvas.style.width = width;
-    Canvases.set(id, canvas)
-    const graph = new cosmos.Graph(canvas)
-    Graphs.set(id, graph)
-}
